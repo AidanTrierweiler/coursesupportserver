f80af7ceca6afcf6b1e9734ce943a9a89ce0f80a
Toby Dragon <dr.toby.dragon@gmail.com>
2022-04-21 16:03:07 -0400

    Added new tests

--- src/test/java/edu/ithaca/dragon/coursesupportserver/AttendanceMarkRepositoryTest.java
+++ src/test/java/edu/ithaca/dragon/coursesupportserver/AttendanceMarkRepositoryTest.java
@@ -28,6 +28,16 @@ public class AttendanceMarkRepositoryTest {
         basicTestRepo.save(new AttendanceMark("Jose", "COMP172", 1, "present"));
         basicTestRepo.save(new AttendanceMark("Monica", "COMP172", 1, "present"));
         basicTestRepo.save(new AttendanceMark("Katie", "COMP172", 1, "present"));
+
+        basicTestRepo.save(new AttendanceMark("Katie", "COMP220", 2, "absent"));
+        basicTestRepo.save(new AttendanceMark("Belinda", "COMP220", 2, "present"));
+        basicTestRepo.save(new AttendanceMark("Jose", "COMP220", 2, "absent"));
+        basicTestRepo.save(new AttendanceMark("Aaron", "COMP220", 2, "present"));
+        basicTestRepo.save(new AttendanceMark("Kaitlyn", "COMP220", 2, "absent"));
+        basicTestRepo.save(new AttendanceMark("Jocelyn", "COMP172", 2, "present"));
+        basicTestRepo.save(new AttendanceMark("Jose", "COMP172", 2, "absent"));
+        basicTestRepo.save(new AttendanceMark("Monica", "COMP172",2, "present"));
+        basicTestRepo.save(new AttendanceMark("Katie", "COMP172", 2, "present"));
     }
 
     @Test
@@ -35,17 +45,36 @@ public class AttendanceMarkRepositoryTest {
         assertTrue(basicTestRepo.findAll().isEmpty());
         loadBasicTestRepo();
         List<AttendanceMark> attendanceMarks = basicTestRepo.findAll();
-        assertEquals(9, attendanceMarks.size());
+        assertEquals(18, attendanceMarks.size());
     }
 
     @Test
     public void testFindByCourseId(){
         loadBasicTestRepo();
-        List<AttendanceMark> attendanceMarks220 = basicTestRepo.findByCourseId("COMP220");
-        assertEquals(5, attendanceMarks220.size());
-        List<AttendanceMark> attendanceMarks172 = basicTestRepo.findByCourseId("COMP172");
-        assertEquals(4, attendanceMarks172.size());
+        assertEquals(10, basicTestRepo.findByCourseId("COMP220").size());
+        assertEquals(8, basicTestRepo.findByCourseId("COMP172").size());
+
+        assertEquals(0, basicTestRepo.findByCourseId("nonpresent course id").size());
+    }
+
+    @Test
+    public void testFindByStudentId(){
+        loadBasicTestRepo();
+        assertEquals(4, basicTestRepo.findByStudentId("Katie").size());
+        assertEquals(4, basicTestRepo.findByStudentId("Jose").size());
+        assertEquals(2, basicTestRepo.findByStudentId("Belinda").size());
+        assertEquals(2, basicTestRepo.findByStudentId("Monica").size());
+
+        assertEquals(0, basicTestRepo.findByStudentId("nonpresent name").size());
     }
     
+    @Test
+    public void testFindByDayNumber(){
+        loadBasicTestRepo();
+        assertEquals(9, basicTestRepo.findByDayNumber(1).size());
+        assertEquals(9, basicTestRepo.findByDayNumber(2).size());
+
+        assertEquals(0, basicTestRepo.findByDayNumber(3).size());
+    }
 
 }
