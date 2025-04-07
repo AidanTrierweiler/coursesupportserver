f2181587e833a294b0929356126071d7bd1be8c5
AidanTrierweiler <atrierweiler@ithaca.edu>
2025-04-07 12:50:46 -0400

    server running but still no data in database

--- src/main/java/edu/ithaca/dragon/coursesupportserver/SQLiteDialect.java
+++ src/main/java/edu/ithaca/dragon/coursesupportserver/SQLiteDialect.java
@@ -33,6 +33,21 @@ public class SQLiteDialect extends Dialect {
 
     @Override
     public String getAddColumnString() {
-        throw new UnsupportedOperationException("No add column syntax supported by SQLiteDialect");
+        return "add column"; // SQLite supports adding columns in a limited way
+    }
+
+    @Override
+    public String getDropForeignKeyString() {
+        throw new UnsupportedOperationException("SQLite does not support dropping foreign keys");
+    }
+
+    @Override
+    public boolean supportsIfExistsBeforeTableName() {
+        return true; // SQLite supports "IF EXISTS" before table names
+    }
+
+    @Override
+    public boolean supportsSequences() {
+        return false; // SQLite does not support sequences
     }
 }
