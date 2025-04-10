bf19e3178dffcfa9b1a63288c05d5489bc54273c
AidanTrierweiler <atrierweiler@ithaca.edu>
2025-04-10 16:50:12 -0400

    updated dependencies and import file to not use id and let sqlite do it

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
-INSERT INTO students (id, netpass, preferred_name) VALUES (1, 'john123', 'John Doe');
-INSERT INTO students (id, netpass, preferred_name) VALUES (2, 'jane456', 'Jane Smith');
+INSERT INTO students (netpass, preferred_name) VALUES ('john123', 'John Doe');
+INSERT INTO students (netpass, preferred_name) VALUES ('jane456', 'Jane Smith');
